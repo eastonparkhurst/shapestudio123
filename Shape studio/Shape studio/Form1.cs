bb1b4646ed1da54b02839b0874a6bcec9eb66e2a
Easton Parkhurst <eastonparkhurst@gmail.com>
2018-04-18 08:54:03 -0600

    for kaden yo

--- Shape studio/Shape studio/Form1.cs
+++ Shape studio/Shape studio/Form1.cs
@@ -15,6 +15,29 @@ namespace Shape_studio
         public Form1()
         {
             InitializeComponent();
+            this.Width = 500;
+            this.Height = 500;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Black);
+            Brush myBrush = new SolidBrush(Color.Blue);
+
+            g.DrawRectangle(myPen, 100, 100, 200, 150);
+            g.FillRectangle(myBrush, 100, 100, 200, 150);
+
+
+
+      
+
         }
     }
 }
